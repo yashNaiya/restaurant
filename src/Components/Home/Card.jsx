74ae492b030a0b3814726bd329038e0dca1b89f9
yashNaiya <yashnaiya16@gmail.com>
2023-01-14 10:52:33 +0530

    dynamic product display

--- src/Components/Home/Card.jsx
+++ src/Components/Home/Card.jsx
@@ -1,6 +1,6 @@
 import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import useForceUpdate from 'use-force-update';
@@ -10,58 +10,80 @@ const Card = (props) => {
     const [counter, setCounter] = useState(0)
     const [add, setAdd] = useState(0)
     useEffect(() => {
-      
+        console.log('mounted')
     }, [add])
-    
-    return (
-        <Box width={'220px'} marginBottom={'1rem'} minHeight={'250px'} borderRadius={'12px'} sx={{ boxShadow: "rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px" }} >
-            <Box
-                height={'50%'}
-                sx={{
-                    backgroundImage: `url(${props.image})`,
-                    backgroundSize: "cover",
-                    backgroundPosition: 'center'
-                }}
+    const clicked = ()=>{
+    }
+    if (props.item) {
+        return (
+            <Box  display={'flex'} flexDirection={'column'} justifyContent={'space-evenly'}
             >
-            </Box>
-            <Box p={1} >
-                <Box display={'flex'} flexDirection={'row'} justifyContent={'space-between'} alignItems={'center'}
-                >
-                    <Typography fontSize={'20px'} color={'primary.main'}>Product name</Typography>
-                    <Typography color={'secondary.main'}>13.99$</Typography>
-                </Box>
-                <Typography variant='caption'>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate.
-                </Typography>
-                <Box marginTop={'.5rem'} width={'100%'} display={'flex'} justifyContent={'space-around'}>
-                    {add === 0 &&
-                        <Button
-                            variant='contained'
-                            sx={{ paddingX: '1.2rem' }} onClick={() => { setAdd(1); setCounter(1); }}
+                <Box width={'250px'} marginBottom={'1rem'} minHeight={'280px'} borderRadius={'12px'}
+                    display={'flex'} flexDirection={'column'} justifyContent={'space-between'}
+                    paddingY={'1rem'}
+                    sx={{ boxShadow: "rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px" }} >
+                    <Box
+                        onClick={clicked}
+                        minHeight={'50%'}
+                        sx={{
+                            ":hover":{cursor:'pointer',opacity:'.7'},
+                            backgroundImage: `url(${props.image})`,
+                            backgroundSize: "cover",
+                            backgroundPosition: 'center'
+                        }}
+                    >
+                    </Box>
+                    <Box p={1} display={'flex'} flexDirection={'column'} maxHeight={'auto'} >
+                        <Box display={'flex'} flexDirection={'row'} justifyContent={'space-between'} alignItems={'center'}
                         >
-                            Add
-                        </Button>}
-                    {add === 1 &&
-                        <Box p={'.3rem'} borderRadius={'.2rem'} border={'1px solid #a9927d'} display={'flex'} flexDirection={'row'}>
-                            <Button onClick={() => {
-                                if(counter === 1)
-                                { setAdd(0)
-                                  forceUpdate() }
-                                else{
-                                    setCounter(counter - 1)
-                                }
-                            }} sx={{ minHeight: 0, minWidth: 0, padding: 0 }}>
-                                <RemoveIcon />
-                            </Button>
-                            <Typography>{counter}</Typography>
-                            <Button onClick={() => { setCounter(counter + 1) }} sx={{ minHeight: 0, minWidth: 0, padding: 0 }}>
-                                <AddIcon />
-                            </Button>
-                        </Box>}
+                            <Typography fontSize={'20px'} maxWidth={'60%'} color={'primary.main'}>{props.item.name}</Typography>
+                            <Typography color={'secondary.main'}>{props.item.price}$</Typography>
+                        </Box>
+                        <Box height={'1.3rem'} overflow={'clip'} >
+                            <Typography variant='caption'>
+                                {props.item.desc}
+                            </Typography>
+                        </Box>
+                    </Box>
+                    <Box marginTop={'.5rem'} width={'100%'} display={'flex'} justifyContent={'space-around'}>
+                        {add === 0 &&
+                            <Button
+                                variant='contained'
+                                sx={{ paddingX: '1.2rem' }} onClick={() => { setAdd(1); setCounter(1); }}
+                            >
+                                Add
+                            </Button>}
+                        {add === 1 &&
+                            <Box p={'.3rem'} borderRadius={'.2rem'} border={'1px solid #a9927d'} display={'flex'} flexDirection={'row'}>
+                                <Button onClick={() => {
+                                    if (counter === 1) {
+                                        setAdd(0)
+                                        forceUpdate()
+                                    }
+                                    else {
+                                        setCounter(counter - 1)
+                                    }
+                                }} sx={{ minHeight: 0, minWidth: 0, padding: 0 }}>
+                                    <RemoveIcon />
+                                </Button>
+                                <Typography>{counter}</Typography>
+                                <Button onClick={() => { setCounter(counter + 1) }} sx={{ minHeight: 0, minWidth: 0, padding: 0 }}>
+                                    <AddIcon />
+                                </Button>
+                            </Box>}
+                    </Box>
                 </Box>
             </Box>
-        </Box>
-    )
+
+        )
+
+    }
+    else {
+        return (
+            <Typography>product</Typography>
+        )
+    }
+
 }
 
 export default Card
--- src/Components/Home/Menu.jsx
+++ src/Components/Home/Menu.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 import Card from './Card'
 import { Collapse, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
@@ -8,83 +8,125 @@ import ExpandMore from '@mui/icons-material/ExpandMore';
 import image from '../../Assets/pizza.jpg'
 import burger from '../../Assets/burger.jpg'
 import { Link } from 'react-scroll'
-
+import { useEffect, useState } from 'react';
+import api from '../../Api';
 const Menu = () => {
     const [open, setOpen] = React.useState(false);
-
+    const [products, setproducts] = useState()
+    const [pizzas, setpizzas] = useState(0)
+    let pizzasTemp=[]
+    const [burgers, setburgers] = useState()
+    let burgersTemp=[]
     const handleClick = () => {
         setOpen(!open);
     };
-    return (
-        <Box
-            maxWidth={'60%'}
-            margin="50px auto"
-            minHeight={'100vh'}
-            display={'flex'}
-            justifyContent={'space-around'}
-            flexDirection={'column'}>
-            <Box width={'300px'}>
-                <ListItemButton onClick={handleClick}>
-                    <ListItemIcon>
-                        <InboxIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Menu" />
-                    {open ? <ExpandLess /> : <ExpandMore />}
-                </ListItemButton>
-                <Collapse in={open} timeout="auto" unmountOnExit>
-                    <List component="div" disablePadding>
-                        <Link to="pizza" spy={true} smooth={true} offset={50} duration={500}>
-                            <ListItemButton sx={{ pl: 4 }}>
-                                <ListItemText primary="Pizza" />
-                            </ListItemButton>
-                        </Link>
-                        <Link to="burger" spy={true} smooth={true} offset={50} duration={500}>
-                            <ListItemButton sx={{ pl: 4 }}>
-                                <ListItemText primary="Burger" />
-                            </ListItemButton>
-                        </Link>
-                    </List>
-                </Collapse>
-            </Box>
-
+    const getproducts = ()=>{
+        api.get('/getproducts')
+        .then(res=>{
+            setproducts(res.data)
+        })
+        if(products){
+            pizzasTemp = products.filter(product=>{
+                return(
+                    product.category === 'pizza'
+                )
+            })
+            burgersTemp = products.filter(product=>{
+                return(
+                    product.category === 'burger'
+                )
+            })
+        }
+    }
+    useEffect(() => {
+        getproducts()
+        if(pizzasTemp && burgersTemp){
+            setpizzas(pizzasTemp)
+            setburgers(burgersTemp)
+            // console.log(pizzas)
+        }
+    }, [products])
+    if(pizzas && burgers){
+        return (
             <Box
-                id={'pizza'}
-                borderTop={'2px solid #a9927d'}
-                paddingTop={8}
+                maxWidth={'60%'}
+                minWidth={'60%'}
                 margin="50px auto"
                 minHeight={'100vh'}
                 display={'flex'}
-                flexDirection={'row'}
-                flexWrap={'wrap'}
-                justifyContent={'space-evenly'}
-            >
-                <Card image={image} />
-                <Card image={image} />
-                <Card image={image} />
-                <Card image={image} />
-                <Card image={image} />
-
+                justifyContent={'space-around'}
+                flexDirection={'column'}>
+                <Box width={'300px'}>
+                    
+                    <ListItemButton onClick={handleClick}>
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        
+                        <ListItemText primary="Menu" />
+                        {open ? <ExpandLess /> : <ExpandMore />}
+                    </ListItemButton>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <List component="div" disablePadding>
+                            <Link to="pizza" spy={true} smooth={true} offset={50} duration={500}>
+                                <ListItemButton sx={{ pl: 4 }}>
+                                    <ListItemText primary="Pizza" />
+                                </ListItemButton>
+                            </Link>
+                            <Link to="burger" spy={true} smooth={true} offset={50} duration={500}>
+                                <ListItemButton sx={{ pl: 4 }}>
+                                    <ListItemText primary="Burger" />
+                                </ListItemButton>
+                            </Link>
+                        </List>
+                    </Collapse>
+                </Box>
+    
+                <Box
+                    id={'pizza'}
+                    borderTop={'2px solid #a9927d'}
+                    paddingTop={8}
+                    margin="50px auto"
+                    display={'flex'}
+                    width={'100%'}
+                    flexDirection={'row'}
+                    flexWrap={'wrap'}
+                    justifyContent={'space-evenly'}
+                >
+                    {pizzas.map(pizza=>{
+                        return(
+                            // <Typography>hii</Typography>
+                            <Card key={pizza._id} image={image} item={pizza} />
+                        )
+                    })}
+                </Box>
+                <Box
+                    id={'burger'}
+                    borderTop={'2px solid #a9927d'}
+                    paddingTop={8}
+                    margin="50px auto"
+                    minHeight={'100vh'}
+                    display={'flex'}
+                    width={'100%'}
+                    flexDirection={'row'}
+                    flexWrap={'wrap'}
+                    justifyContent={'space-evenly'}
+                >
+                    {burgers.map(burg=>{
+                        return(
+                            
+                            <Card key={burg._id} image={burger} item={burg} />
+                        )
+                    })}
+                </Box>
+    
             </Box>
-            <Box
-                id={'burger'}
-                borderTop={'2px solid #a9927d'}
-                paddingTop={8}
-                margin="50px auto"
-                minHeight={'100vh'}
-                display={'flex'}
-                flexDirection={'row'}
-                flexWrap={'wrap'}
-                justifyContent={'space-evenly'}
-            >
-                <Card image={burger} />
-                <Card image={burger} />
-                <Card image={burger} />
-                <Card image={burger} />
-                <Card image={burger} />
-            </Box>
-
-        </Box>
-    )
+        )
+    }else{
+        return(
+            <Typography>Loading...</Typography>
+        )
+    }
 }
 
 export default Menu
